0951bd5e444811d8efbb0dde8c7f7e626f268a5b
jeroen.stiekema <jeroen.stiekema@gino.nl>
2013-10-15 22:02:46 +0200

    * prepared maven archetype

---
+++ archetype-springmvc/src/main/resources/archetype-resources/src/test/java/eu/stiekema/jeroen/archetypes/springmvc/security/PasswordEncoderTest.java
@@ -0,0 +1,25 @@
+package eu.stiekema.jeroen.archetypes.springmvc.security;
+
+import java.util.Date;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author Jeroen Stiekema
+ */
+public class PasswordEncoderTest {
+
+    @Test
+    @Ignore
+    public void bCryptPasswordEncoderTest() {
+        for (int rounds = 10; rounds < 31; rounds++) {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(rounds);
+            long start = new Date().getTime();
+            String password = encoder.encode("bob123");
+            long end = new Date().getTime();
+            System.out.println("encoding password with " + rounds +" rounds: " + (end - start));
+            System.out.println(password);
+        }
+    }
+}
